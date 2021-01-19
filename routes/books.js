a99665884776fb4cb759b358aeb5b7d6f9974cc1
David Oster <paspa@hotmail.com>
2021-01-19 22:22:54 +0200

    Updated for new book
    - added the router.post
    - added the new view books_new

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var dbconnection = require('../lib/db');
 
+// get shows a page of Books (SELECT)
+// post sends data to backend (INSERT)
+// put updates a current record (UPDATE)
+// delete deletes a current record (DELETE)
+
 /* GET home page. */
 // URL: http://localhost:3000/books/
 router.get('/', function(req, res, next) {
@@ -16,6 +21,10 @@ router.get('/', function(req, res, next) {
 });
 
 // http://localhost:3000/books/add
-  
+router.get('/add/', function(req, res, next) {
+    res.render('books_new', { title: 'Books - Add New' });
+});
+
+// router.post('/add/',....)
 module.exports = router;
   

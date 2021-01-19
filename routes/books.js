901c604dd3f8967066311517983fc36fa74baa73
David Oster <paspa@hotmail.com>
2021-01-19 21:04:32 +0200

    updated for showing the list of books in the db
    - created the routes.get(/) where it returns a render section with db rows
    - if the returnd db rows is empty an empty object is returned

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var dbconnection = require('../lib/db');
 
 /* GET home page. */
 // URL: http://localhost:3000/books/
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Books' });
+    const query = "SELECT * FROM books";
+    dbconnection.query(query, function(err, rows) {
+        if(err) {
+            res.render('books', { title: 'Books - ERROR', books: '' });
+        } else {
+            res.render('books', { title: 'Books', books:rows });
+        }
+    });
 });
   
 module.exports = router;

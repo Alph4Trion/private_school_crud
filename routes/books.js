8e5d11caaea5487b067c5eac0d24ea3531c0deb1
David Oster <paspa@hotmail.com>
2021-01-20 21:23:54 +0200

    finally implemented the right way a POST!!!!

--- routes/books.js
+++ routes/books.js
@@ -2,29 +2,51 @@ var express = require('express');
 var router = express.Router();
 var dbconnection = require('../lib/db');
 
-// get shows a page of Books (SELECT)
-// post sends data to backend (INSERT)
-// put updates a current record (UPDATE)
-// delete deletes a current record (DELETE)
+// get shows a page of Books (SELECT) R
+// post sends data to backend (INSERT) C 
+// put updates a current record (UPDATE) U 
+// delete deletes a current record (DELETE) D
 
 /* GET home page. */
 // URL: http://localhost:3000/books/
-router.get('/', function(req, res, next) {
+router.get('/list/:message?', function(req, res, next) {
     const query = "SELECT * FROM books";
+    console.log("outside" + req.params.message);
     dbconnection.query(query, function(err, rows) {
+        console.log("inside" + req.params.message);
         if(err) {
-            res.render('books', { title: 'Books - ERROR', books: '' });
+            res.render('books', { title: 'Books - ERROR', books: '', message: req.params.message });
         } else {
-            res.render('books', { title: 'Books', books:rows });
+            res.render('books', { title: 'Books', books:rows, message: req.params.message });
         }
     });
 });
 
 // http://localhost:3000/books/add
+// shows the actual form for the model book in order to
+// collect data from the  client and send them to the backend
 router.get('/add/', function(req, res, next) {
-    res.render('books_new', { title: 'Books - Add New' });
+    res.render('books_new', { title: 'Books - Add New', message:'' });
 });
 
-// router.post('/add/',....)
+router.post('/add', function(req, res, next) {
+    const query = "INSERT INTO `books`(`title`, `author`) VALUES('"+ req.body.title + "', '" + req.body.author + "')";
+    console.log(query);
+    dbconnection.query(query, function(err, status) {
+        // NOT OK - Error!!!
+        if(err) {
+            res.render("books_new", { title: 'Books - Add New', message: "Error inserting data to the database!" });
+        } 
+        // All OK!!!
+        else {
+            //res.render("books", { title: 'Books', books: '', message: "All ok!!!" });
+            // res.render("books", {});
+            res.redirect('/books/list/All OK!!!');
+        }
+        
+    });
+});
+
+
 module.exports = router;
   

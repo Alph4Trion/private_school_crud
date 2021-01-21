8060b1b7301695425a896f9936a71df110bb5c54
David Oster <paspa@hotmail.com>
2021-01-21 22:29:56 +0200

    final

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // const {x} = 
+  res.render('index', { title: 'Express', title2: 'CB12 PT JavaScript' });
 });
 
 module.exports = router;
